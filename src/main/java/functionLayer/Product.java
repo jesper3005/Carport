8b90d07c5e5e3fb1d5beeb70c08181862df62c77
Jesper <jesper300595@gmail.com>
2018-11-15 21:52:01 +0100

    Fix

--- src/main/java/functionLayer/Product.java
+++ src/main/java/functionLayer/Product.java
@@ -11,4 +11,17 @@ package functionLayer;
  */
 public class Product {
     
+    int id;
+    String name, category;
+    int price, length, width, height;    
+
+    public Product(int id, String name, String category, int price, int length, int width, int height) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
 }
