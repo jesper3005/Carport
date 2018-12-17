d9f286cc55748689fc2af07f8eec40428d6d6330
Jörg <oerteljoerg@gmail.com>
2018-12-17 20:05:44 +0100

    classNotFound

--- src/main/java/functionLayer/Rules.java
+++ src/main/java/functionLayer/Rules.java
@@ -10,8 +10,7 @@ package functionLayer;
  * @author Jesper
  */
 public class Rules {
-    
-    
+
     public static final double REM_WIDTH = 10;
     public static final double REM_HEIGHT = 30;
     public static final double PLANKS_WIDTH = 10;
@@ -22,13 +21,11 @@ public class Rules {
     public static final double POLES_GAP_REM = 10;
     public static final double SHED_PLANK_WIDTH = 15;
     public static final double DOOR_WIDTH = 80;
-<<<<<<< HEAD
-=======
     public static final double DISTANCE_BETWEEN_EACH_BATTENS = 50;
     public static final double POINTED_ROOF_SURPLUS = 25;
     public static final double AMOUNT_OF_BEAMS_OVER_OR_EQUAL_540 = 5;
     public static final double AMOUNT_OF_BEAMS_UNDER_540 = 4;
     public static final double AMOUNT_OF_POLES_LESS_OR_EQUAL_450 = 4;
     public static final double AMOUNT_OF_POLES_OVER_450 = 6;
->>>>>>> 6f9960f552c67bf1c25feef3b938128f32b9d4cf
+    
 }
